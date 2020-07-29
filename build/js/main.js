01890a086ee5e626b023aee1a8b3dd6159781339
Sergey Klimov <desire0417@gmail.com>
2020-07-29 15:56:22 +0300

    добавляет верстку блока "questions"

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,26 @@
 'use strict';
 (function () {
+  var questionsList = document.querySelector('.questions__list');
+  var questionsButtons = questionsList.querySelectorAll('button');
+
+  var onClickQuestionButton = function (evt) {
+    var parentElement = evt.target.parentElement.parentElement;
+    var activeClass = 'questions__item--active';
+
+    if (parentElement.classList.contains(activeClass)) {
+      parentElement.classList.remove(activeClass);
+    } else {
+      parentElement.classList.add(activeClass);
+    }
+  };
+
+  questionsButtons.forEach(function (it) {
+    it.addEventListener('click', onClickQuestionButton);
+  });
+})();
+
+'use strict';
+(function () {
   var openPopupButton = document.querySelector('.header__information-order');
   var popup = document.querySelector('.popup');
   var closePopupButtons = popup.querySelectorAll('.popup__close');
